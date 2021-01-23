5571df6cd36abcf1f4ee0fc5be4e77289f13d4c3
MoltenCoreDev <craxell.tv@gmail.com>
2021-01-23 01:13:05 +0100

    Release 1.0.1-alpha soon // making powerush shell soon tm

--- bootloaderofnoosthisisapointlesslongname.asm
+++ bootloaderofnoosthisisapointlesslongname.asm
@@ -1,3 +1,47 @@
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'f'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'm'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'n'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 't'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'a'
+int 0x10
+mov al, 'm'
+int 0x10
+
+
+
+
 loop:
     jmp loop 
 times 510-($-$$) db 0
