9374f6632daaa1b62494935871b22f4224e45351
MoltenCoreDev <craxell.tv@gmail.com>
2021-01-23 15:58:19 +0100

    Added stack print

--- bootloaderofnoosthisisapointlesslongname.asm
+++ bootloaderofnoosthisisapointlesslongname.asm
@@ -1,45 +1,40 @@
 mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'f'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'm'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'O'
-int 0x10
-mov al, 'S'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 't'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'm'
-int 0x10
 
+mov bp, 0x8000
+mov sp, bp
 
+push '^'
+push 'm'
+push 'a'
+push 'e'
+push 't'
+push ' '
+push 'S'
+push 'O'
+push 'o'
+push 'n'
+push ' '
+push 'm'
+push 'o'
+push 'r'
+push 'f'
+push ' '
+push 'o'
+push 'l'
+push 'l'
+push 'l'
+push 'e'
+push 'H'
+
+print:
+	pop bx
+	mov al, bl
+	cmp al, '^'
+	je exit
+	int 0x10
+	jmp print
+exit:
+	
 
 
 loop:
