59d2c604827d555924d455553a98608d081985fc
MoltenCoreDev <craxell.tv@gmail.com>
2021-01-23 16:05:33 +0100

    Fix git being git

--- bootloaderofnoosthisisapointlesslongname.asm
+++ bootloaderofnoosthisisapointlesslongname.asm
@@ -1,55 +1,7 @@
 mov ah, 0x0e ; tty mode
-<<<<<<< HEAD
 
 mov bp, 0x8000
 mov sp, bp
-=======
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'f'
-int 0x10
-mov al, 'r'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'm'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 'n'
-int 0x10
-mov al, 'o'
-int 0x10
-mov al, 'O'
-int 0x10
-mov al, 'S'
-int 0x10
-mov al, ' '
-int 0x10
-mov al, 't'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'a'
-int 0x10
-mov al, 'm'
-int 0x10
-mov al, ' '
-int 0x10
-mov al,':'
-int 0x10
-mov al,'D'
-int 0x10
->>>>>>> 09279106720e8e389d9748c37d4f28367e11a1af
 
 push '^'
 push 'm'
