d62e691a12328386303592f88cf113aff6f9b85d
MoltenCoreDev <craxell.tv@gmail.com>
2021-01-27 18:25:22 +0100

    If this commit goes through pls revert quik

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 rm -rf build
 mkdir build
+sudo apt install git -y
+git clone https://github.com/NOperatingSystem/Dripman.git
+cd Dripman
+git pull origin master
+cd ..
 ./nasm_linux_x64 -f bin ./boot_sect.cum -o ./build/boot_sect.bin && echo "Built Successfully. Now, use qemu x86_64 to run the file in the build/ directory."
