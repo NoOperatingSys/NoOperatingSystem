3f095104f256c6108716d050f398b0b6c2ea7e41
Mugilan N <53713683+nature174@users.noreply.github.com>
2021-01-29 11:56:33 -0800

    comments

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
 rm -rf build
 mkdir build
-./nasm_linux_x64 -f bin boot.asm -o ./build/boot.bin && echo "Built Successfully, Running.\nPress Ctrl +A and then X to exit" && read junk && qemu-system-x86_64 -nographic ./build/boot.bin
+./nasm_linux_x64 -f bin boot.asm -o ./build/boot.bin && echo "Built Successfully, Running.\nPress Ctrl +A and then X to exit on Linux or Ctrl+C on Windows [press enter]" && read junk && qemu-system-x86_64 -nographic ./build/boot.bin
