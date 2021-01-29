3bf71467b78220e76745e272c00baf5ad7c037f7
Jai L <47060357+jdadonut@users.noreply.github.com>
2021-01-29 13:50:34 -0500

    so like what if it was button but not working?

--- tools/run.bat
+++ tools/run.bat
@@ -1,4 +1,4 @@
 @echo off
 rmdir build /f
 mkdir build
-nasm -f bin src/boot_sect.cum -o ./build/boot_sect.bin && echo "Built Successfully, Running..." && qemu-system-x86_64 ./build/boot_sect.bin
+nasm -f bin src/boot.asm -o ./build/boot.bin && echo "Built Successfully, Running..." && qemu-system-x86_64 ./build/boot.bin
