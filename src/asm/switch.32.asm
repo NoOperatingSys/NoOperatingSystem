ce61011927083062e8ed8695e521f3b85ba9de4d
Mugilan N <53713683+nature174@users.noreply.github.com>
2021-01-30 21:19:01 -0800

    ep

--- src/asm/switch.32.asm
+++ src/asm/switch.32.asm
@@ -3,7 +3,7 @@ pm:
     cli ; disable interrupts
     lgdt [gdt_descriptor] ; load the gtd descriptor
     mov eax, cr0
-    or eax, 0x1 ;set 32-bit mode in cr0
+    or al, 0x1 ;set 32-bit mode in cr0
     mov cr0, eax
     jmp CODE_SEG:init_pm ;far jump by using a different segment
 
@@ -18,9 +18,6 @@ init_pm:
     mov es, ax
     mov fs, ax
     mov gs, ax
-
-    ;mov ebp, 0x90000 
     mov esp, 0x90000 
-    ;mov esp, ebp
 
     call BEGIN_PM ;yes moment
