a89efa90f809c4de29c55fefc416696b0545715e
MoltenCoreDev <molters.tv@gmail.com>
2022-01-20 21:34:55 +0100

    fixed indentation :)

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,11 +1,10 @@
-; TODO: fix formatting
 bits 32
 section .text
-        ;multiboot spec
-        align 4
-        dd 0x1BADB002            ;magic
-        dd 0x00                  ;flags
-        dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
+	;multiboot spec
+	align 4
+	dd 0x1BADB002            ;magic
+	dd 0x00                  ;flags
+	dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
 extern kmain            ;kmain is defined in the c file
